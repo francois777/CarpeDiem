ba1f25c6bf68fd6f978ee43df217f7f5568a9a14
Francois van der Hoven <francois777@optusnet.com.au>
2014-11-26 13:33:53 +1000

    new banner photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def to_afrikaans
+    Carpediem::Application.change_locale(:af)
+    # Rails.configuration.i18n.default_locale = :af
+    # I18n.locale = I18n.default_locale = Rails.configuration.i18n.locale = Rails.configuration.i18n.default_locale
+    # puts "Locale is now set to #{I18n.locale}"
+    redirect_to '/'
+    # render nothing: true
+  end
+end
+
+module Carpediem
+  class Application
+    def change_locale(new_locale)
+      puts "doing the impossible"
+      config.i18n.default_locale = new_locale
+      config.i18n.locale = new_locale
+    end
+  end
 end
